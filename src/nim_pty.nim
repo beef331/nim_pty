9ae045d78a25ea1a39790646298d11c069a80f53
Jason Beetham <beefers331@gmail.com>
2020-06-03 18:12:57 -0600

    Removed fork

--- src/nim_pty.nim
+++ src/nim_pty.nim
@@ -7,5 +7,4 @@ proc ptsName*(fd : cint): cstring {.importC:"ptsname".}
 proc ptsNameR*(fd : cint, buffer : ptr cstring, bufLen : csize_t) : int{.importC:"ptsname_r".}
 proc grantPt*(fd : cint): cint {.importC: "grantpt".}
 proc unlockPt*(fd : cint): cint {.importC: "unlockpt".}
-proc fork*(toFork : void): cint {.importC: "fork".}
 proc dup2*(oldFd, newFd: cint): cint {.importC: "dup2".}
